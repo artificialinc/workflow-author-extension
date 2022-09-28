d718816af0bd2b582b68ecc981b54eace4a951fa
artificial-smahon <shawn@artificial.com>
2022-09-28 17:42:36 -0500

    feat: generate assistant stubs

--- src/generateActionStubs.ts
+++ src/generateActionStubs.ts
@@ -4,31 +4,102 @@ import * as path from 'path';
 import { FunctionSignature } from './types';
 import { pathExists } from './utils';
 import { BuildFunctionSignatures } from './buildFunctionSignatures';
-import { ArtificialApollo } from './apollo';
+import { ArtificialApollo, Assistant, AssistantTypeInfo } from './apollo';
 import { OutputLog } from './outputLog';
+import { snakeCase } from 'lodash';
+import { type } from 'os';
 
 export class GenerateActionStubs {
   outputChannel = OutputLog.getInstance();
   constructor(private workspaceRoot: string) {}
   async generateStubs(): Promise<any> {
-    let terminal = vscode.window.activeTerminal;
-    if (!terminal) {
-      terminal = vscode.window.createTerminal(`Ext Terminal`);
-    }
-    terminal.sendText('cd workflow');
-    terminal.sendText('ls');
+    this.generatePythonStubs();
+    await this.generateAssistantStubs();
+  }
 
+  private async generateAssistantStubs(): Promise<void> {
     const client = ArtificialApollo.getInstance();
     const response = await client.queryAssistants();
     this.outputChannel.log(JSON.stringify(response));
 
+    let pythonContent = '# GENERATED FILE: DO NOT EDIT BY HAND\n';
+    pythonContent += '# REGEN USING EXTENSION\n';
+    pythonContent += 'from typing import List\n\n';
+    pythonContent += 'from artificial.workflows.decorators import assistant, parameter\n\n';
+    if (!response?.assistants) {
+      return;
+    }
+    for (const sig of response?.assistants) {
+      pythonContent += `@assistant('${sig.id}')\n`;
+      pythonContent += this.buildAssistantParmDec(sig);
+      pythonContent += `async def assistant_${snakeCase(sig.name)}(\n`;
+      pythonContent += this.buildAssistantParams(sig);
+      pythonContent += `) -> None:\n`;
+      pythonContent += `\tpass\n\n`;
+    }
+
+    fs.writeFile(path.join(this.workspaceRoot, 'workflow', 'stubs_assistants.py'), pythonContent, (err) => {
+      if (err) {
+        return vscode.window.showErrorMessage('Failed to create boilerplate file!');
+      }
+      vscode.window.showInformationMessage('Created boilerplate files');
+    });
+  }
+
+  private buildAssistantParams(sig: Assistant): string {
+    let returnString = '';
+    for (const parm of sig.parameters) {
+      returnString += `\targ_${snakeCase(parm.typeInfo.name)}: ${this.convertToPythonType(parm.typeInfo)},\n`;
+    }
+    return returnString;
+  }
+
+  private convertToPythonType(type: AssistantTypeInfo | { type: string }): string {
+    let returnString = '';
+    if (type.type === 'ARRAY') {
+      if ('subTypes' in type) {
+        returnString += `List[${this.convertToPythonType(type.subTypes[0])}]`;
+      }
+    } else {
+      switch (type.type) {
+        case 'INT':
+          returnString += 'int';
+          break;
+        case 'FLOAT':
+          returnString += 'float';
+          break;
+        case 'BOOLEAN':
+          returnString += 'bool';
+          break;
+        case 'STRING':
+          returnString += 'str';
+          break;
+        case 'EQUIPMENT_REF':
+          returnString += 'str';
+          break;
+      }
+    }
+    return returnString;
+  }
+
+  private buildAssistantParmDec(sig: Assistant): string {
+    let returnString = '';
+    for (const parm of sig.parameters) {
+      returnString += `@parameter('arg_${snakeCase(parm.typeInfo.name)}', action_parameter_name='${
+        parm.typeInfo.name
+      }')\n`;
+    }
+    return returnString;
+  }
+
+  private generatePythonStubs(): void {
     let funcSigs: FunctionSignature[] = [];
     const actionPythonPath = path.join(this.workspaceRoot, 'adapter', 'actions.py');
     if (pathExists(actionPythonPath)) {
       funcSigs = new BuildFunctionSignatures().build(actionPythonPath);
     } else {
       vscode.window.showInformationMessage('Workspace has no actions.py');
-      return Promise.resolve([]);
+      return;
     }
 
     let pythonContent = '# GENERATED FILE: DO NOT EDIT BY HAND\n';
@@ -74,3 +145,10 @@ export class GenerateActionStubs {
     });
   }
 }
+
+// let terminal = vscode.window.activeTerminal;
+// if (!terminal) {
+//   terminal = vscode.window.createTerminal(`Ext Terminal`);
+// }
+// terminal.sendText('cd workflow');
+// terminal.sendText('ls');
