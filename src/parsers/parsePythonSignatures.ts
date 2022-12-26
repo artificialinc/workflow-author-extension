9086e88df2371c71858b670ff0805530a208e7f4
artificial-smahon <shawn@artificial.com>
2022-12-26 00:49:11 -0600

    feat: adapter action stub gen with modules

--- src/parsers/parsePythonSignatures.ts
+++ src/parsers/parsePythonSignatures.ts
@@ -16,35 +16,79 @@ See the License for the specific language governing permissions and
 
 import * as fs from 'fs';
 import { pathExists } from '../utils';
-import { parse, createVisitor, DecoratedContext } from 'python-ast';
+import {
+  parse,
+  createVisitor,
+  DecoratedContext,
+  SuiteContext,
+  StmtContext,
+  FuncdefContext,
+  ParseTree,
+} from 'python-ast';
 import { FunctionSignature, Param } from '../apis/types';
 
 export class BuildPythonSignatures {
-  build(actionPythonPath: string): FunctionSignature[] {
+  build(actionPythonPath: string): { signatures: FunctionSignature[]; module: string } {
     if (pathExists(actionPythonPath)) {
       const pythonStubs = fs.readFileSync(actionPythonPath, 'utf-8');
       const signatureList: FunctionSignature[] = [];
+      let adapterModule = '';
 
       const buildSignature = (source: string) => {
         let ast = parse(source);
 
         return createVisitor({
           visitDecorated: (ast) => {
+            const signature: FunctionSignature = { parameters: [], name: '', returnType: '', moduleName: '' };
             const decoratorName = ast.decorators().decorator(0).dotted_name().text;
             if (decoratorName === 'action' || decoratorName === 'substrate_action') {
-              const signature: FunctionSignature = { parameters: [], name: '', returnType: '' };
               signature.name = this.findFuncName(ast);
               signature.parameters = this.findParamNameAndType(ast);
               signature.returnType = ast?.async_funcdef()?.funcdef().test()?.text ?? '';
               signatureList.push(signature);
             }
           },
+          // visitVarargslist: (ast) => {
+          //   console.log(ast.text);
+          // },
+          visitFuncdef: (ast) => {
+            const funcName = ast.NAME().text;
+            if (funcName === '__init__') {
+              // const findModule = (ast: ParseTree) => {
+              //   return createVisitor({
+              //     // eslint-disable-next-line @typescript-eslint/naming-convention
+              //     visitArglist: (ast) => {
+              //       signature.moduleName = ast.text;
+              //     },
+              //   });
+              // };
+              // findModule(ast);
+
+              for (let x = 0; x < ast.childCount; x++) {
+                if (ast.getChild(x) instanceof SuiteContext) {
+                  const body = ast.getChild(x) as SuiteContext;
+                  for (let y = 0; y < body.childCount; y++) {
+                    if (body.getChild(y) instanceof StmtContext) {
+                      const text = body.getChild(y).text.replace(new RegExp("'", 'g'), '');
+                      if (text.match('super')) {
+                        const module = text.split('"')[1];
+                        if (module !== undefined) {
+                          adapterModule = module;
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
         }).visit(ast);
       };
       buildSignature(pythonStubs);
-      return signatureList;
+
+      return { signatures: signatureList, module: adapterModule };
     } else {
-      return [];
+      return { signatures: [], module: '' };
     }
   }
 
