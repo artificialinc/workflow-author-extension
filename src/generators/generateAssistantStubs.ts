acb4c6643659556139de656b76ba61e4e02ab4b9
artificial-smahon <shawn@artificial.com>
2022-12-30 21:15:54 -0600

    chore: refactor

---
+++ src/generators/generateAssistantStubs.ts
@@ -0,0 +1,146 @@
+/*
+Copyright 2022 Artificial, Inc. 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+ limitations under the License. 
+*/
+
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import { ArtificialApollo, Assistant, AssistantTypeInfo } from '../providers/apolloProvider';
+import { OutputLog } from '../providers/outputLogProvider';
+import { snakeCase } from 'lodash';
+import { AssistantByLabTreeView } from '../views/assistantTreeView';
+import { AssistantSignature, BuildAssistantSignatures } from '../parsers/parseAssistantSignatures';
+import * as _ from 'lodash';
+
+export class GenerateAssistantStubs {
+  outputChannel = OutputLog.getInstance();
+  constructor(private workspaceRoot: string, private assistantByLab: AssistantByLabTreeView) {}
+
+  async generateAssistantStubsCommand(): Promise<any> {
+    await this.generateAssistantStubs();
+    await this.assistantByLab.refresh();
+    vscode.window.showInformationMessage('Created Assistant Stub File');
+  }
+
+  private async generateAssistantStubs(): Promise<void> {
+    const client = ArtificialApollo.getInstance();
+    const response = await client.queryAssistants();
+    const customAssistantStubPath = vscode.workspace.getConfiguration('artificial.workflow.author').assistantStubPath;
+    const fullPath = path.join(this.workspaceRoot, customAssistantStubPath);
+    const stubs = new BuildAssistantSignatures().build(fullPath);
+
+    let pythonContent = '# GENERATED FILE: DO NOT EDIT BY HAND\n';
+    pythonContent += '# REGEN USING EXTENSION\n';
+    pythonContent += '# flake8: noqa\n';
+    pythonContent += 'from typing import List\n\n';
+    pythonContent += 'from artificial.workflows.decorators import assistant, parameter\n\n\n';
+    if (!response?.assistants) {
+      return;
+    }
+
+    for (const sig of response?.assistants) {
+      let allParamsSorted = this.getAllParamsSorted(sig, stubs);
+      pythonContent += `@assistant('${sig.id}')\n`;
+      pythonContent += this.buildAssistantParmDec(sig, allParamsSorted);
+      pythonContent += `async def assistant_${snakeCase(sig.name)}(\n`;
+      pythonContent += this.buildAssistantParams(sig, allParamsSorted);
+      pythonContent += `) -> None:\n`;
+      pythonContent += `    pass\n\n\n`;
+    }
+
+    fs.writeFile(fullPath, pythonContent, (err) => {
+      if (err) {
+        return vscode.window.showErrorMessage('Failed to create boilerplate file!');
+      }
+    });
+  }
+
+  private getAllParamsSorted(apolloSignature: Assistant, stubs: AssistantSignature[]): string[] {
+    let allParamsSorted: string[] = [];
+
+    let stubParams = [];
+    let alabParams = [];
+    if (stubs) {
+      const stubSignature = stubs.find((element) => element.actionId === apolloSignature.id);
+      if (stubSignature) {
+        for (let param of stubSignature?.parameters) {
+          stubParams.push(param.assistantName);
+        }
+      }
+      for (let param of apolloSignature.parameters) {
+        alabParams.push(param.typeInfo.name);
+      }
+      const sortedParams = _.intersection(stubParams, alabParams);
+      allParamsSorted = _.concat(sortedParams, _.difference(alabParams, sortedParams));
+    }
+
+    return allParamsSorted;
+  }
+
+  private buildAssistantParams(sig: Assistant, allParamsSorted: string[]): string {
+    let returnString = '';
+
+    for (const param of allParamsSorted) {
+      const paramSig = sig.parameters.find((element) => element.typeInfo.name === param);
+      if (paramSig) {
+        returnString += `    arg_${snakeCase(paramSig.typeInfo.name)}: ${this.convertToPythonType(
+          paramSig.typeInfo
+        )},\n`;
+      }
+    }
+    return returnString;
+  }
+
+  private convertToPythonType(type: AssistantTypeInfo | { type: string }): string {
+    let returnString = '';
+    if (type.type === 'ARRAY') {
+      if ('subTypes' in type) {
+        returnString += `List[${this.convertToPythonType(type.subTypes[0])}]`;
+      }
+    } else {
+      switch (type.type) {
+        case 'INT':
+          returnString += 'int';
+          break;
+        case 'FLOAT':
+          returnString += 'float';
+          break;
+        case 'BOOLEAN':
+          returnString += 'bool';
+          break;
+        case 'STRING':
+          returnString += 'str';
+          break;
+        case 'EQUIPMENT_REF':
+          returnString += 'str';
+          break;
+      }
+    }
+    return returnString;
+  }
+
+  private buildAssistantParmDec(sig: Assistant, allParamsSorted: string[]): string {
+    let returnString = '';
+    for (const param of allParamsSorted) {
+      const paramSig = sig.parameters.find((element) => element.typeInfo.name === param);
+      if (paramSig) {
+        returnString += `@parameter('arg_${snakeCase(paramSig.typeInfo.name)}', action_parameter_name='${
+          paramSig.typeInfo.name
+        }')\n`;
+      }
+    }
+    return returnString;
+  }
+}
